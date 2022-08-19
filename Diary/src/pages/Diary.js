df9c5e008ca1c4d79b2144a588cf81288fd98031
legojinK <ckgksck@gmail.com>
2022-08-19 18:24:15 +0900

    [init setting] 기본적인 pages

---
+++ Diary/src/pages/Diary.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>Diary</h1>
+    </div>
+  );
+};
+export default Diary;
---
+++ Diary/src/pages/New.js
@@ -0,0 +1,8 @@
+const New = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+export default New;
