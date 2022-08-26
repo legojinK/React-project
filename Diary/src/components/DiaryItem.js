72c0097a8c4a0a5500a2c2ed6850978f1d2d3544
legojinK <ckgksck@gmail.com>
2022-08-26 11:14:32 +0900

    navigate 페이지 이동

--- Diary/src/components/DiaryItem.js
+++ Diary/src/components/DiaryItem.js
@@ -1,20 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 
 const DiaryItem = ({ id, emotion, content, date }) => {
   const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className="DiaryItem">
-      <div className={`emotion_img_wrapper_${emotion}`}>
+      <div
+        className={[
+          "emotion_img_wrapper",
+          `emotion_img_wrapper_${emotion}`,
+        ].join(" ")}
+        onClick={goDetail}
+      >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div className="info_wrapper">
+      <div className="info_wrapper" onClick={goDetail}>
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 35)}</div>
       </div>
       <div className="btn_wrapper">
-        <MyButton text={"Edit"} />
+        <MyButton text={"Edit"} onClick={goEdit} />
       </div>
     </div>
   );
