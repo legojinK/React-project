1f76f1056a37e61f8ecdc3422bbbffdaa59130d2
legojinK <ckgksck@gmail.com>
2022-08-30 09:17:14 +0900

    /Home 에서 data 가져오기

--- Diary/src/pages/Home.js
+++ Diary/src/pages/Home.js
@@ -1,15 +1,75 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryStateContext } from "./../App";
 import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
+import DiaryList from "../components/DiaryList";
+
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()} . ${curDate.getMonth() + 1}`;
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+      console.log(new Date(firstDay));
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0,
+        23,
+        59,
+        59
+      ).getTime();
+      console.log(new Date(lastDay));
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
-    <div>
+    <div className="Home">
       <MyHeader
-      
-        leftChild={<MyButton text={"<"} onClick={} />}
-        rightChild={<MyButton text={">"} onClick={} />}
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
       />
-      <h1>Header</h1>
+      <DiaryList diaryList={data} />
+      <div className="calendar_btn">
+        <MyButton
+          text={"📅 Diary Calendar "}
+          onClick={() => navigate("/date")}
+          type={"positive"}
+        />
+      </div>
     </div>
   );
 };
+
 export default Home;
